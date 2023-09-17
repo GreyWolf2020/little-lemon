603d384f461628f1a9b61c43fb09ed9f2a705aaa
GreyWolf100 <gregphiri95@gmail.com>
2023-09-17 22:03:42 +0200

    Added the Profile Screen displaying the name, surname and email of currently logged in user

---
+++ app/src/main/java/com/example/littlelemon/presentation/ProfileViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.littlelemon.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.littlelemon.data.userprofile.UserProfile
+import com.example.littlelemon.data.userprofile.UserProfileRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class ProfileViewModel(
+    private val userProfileRepository: UserProfileRepository
+) : ViewModel() {
+    private val _userProfile = MutableStateFlow<UserProfile>(UserProfile("", "",""))
+    val userProfile = _userProfile.asStateFlow()
+
+    init {
+        getUserProfile()
+    }
+    private fun getUserProfile() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val userProfile = userProfileRepository.getUserProfile().first()
+            _userProfile.update {
+                it.copy(
+                    name = userProfile.name,
+                    surname = userProfile.surname,
+                    email = userProfile.email
+                )
+            }
+        }
+    }
+    fun removeUserProfile() {
+        viewModelScope.launch(Dispatchers.IO) {
+            userProfileRepository.deleteUserProfile()
+        }
+
+    }
+}
+
