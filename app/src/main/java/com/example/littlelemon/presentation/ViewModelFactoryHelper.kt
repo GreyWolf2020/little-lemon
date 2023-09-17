ad6574dfac4f04f40b9591746c529c5f6c9cefc4
GreyWolf100 <gregphiri95@gmail.com>
2023-09-17 18:47:28 +0200

    Added a  factory ViewModelFactoryHelper to assist to create new viewmodels i the application.

---
+++ app/src/main/java/com/example/littlelemon/presentation/ViewModelFactoryHelper.kt
@@ -0,0 +1,12 @@
+package com.example.littlelemon.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+fun <VM : ViewModel> viewModelFactory(initialzer: () -> VM): ViewModelProvider.Factory {
+    return object : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return initialzer() as T
+        }
+    }
+}
