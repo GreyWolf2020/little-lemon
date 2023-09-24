c47ed4a0c45d2da30057116af93974428a39c7af
GreyWolf100 <gregphiri95@gmail.com>
2023-09-24 22:40:05 +0200

    Have just added the Typograph for the whole app text

--- app/src/main/java/com/example/littlelemon/ui/theme/Type.kt
+++ app/src/main/java/com/example/littlelemon/ui/theme/Type.kt
@@ -2,33 +2,52 @@ package com.example.littlelemon.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.littlelemon.R
+
+val karlaFamily = FontFamily(
+    Font(R.font.karla_regular, FontWeight.Normal)
+)
+
+val markaziTextFamily = FontFamily(
+    Font(R.font.markazitext_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = markaziTextFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 64.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = markaziTextFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 40.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = karlaFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = karlaFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = karlaFamily,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = karlaFamily,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 16.sp
     )
-    */
 )
